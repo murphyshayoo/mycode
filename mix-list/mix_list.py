952ddeeff024f955a787242d63483a5209c051aa
murphyshayoo <murphyadetunji@gmail.com>
2020-10-26 19:01:35 +0000

    my fist python program

---
+++ mix-list/mix_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# I am working on a list called my_list
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#I am trying to print out ip address of 192.168.0.5
+print("The first item in the list (IP): " + my_list[0] )
+
+#Trying to print out the port number
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# Setting port to UP state
+print("The last item in the list (state): " + my_list[2] )
+
